acb261557a23b86dd8dd11111bc36d16a3e8fe83
unknown <dmytrenkoalyona@gmail.com>
2025-07-01 16:35:49 +0300

    start hw

--- src/index.js
+++ src/index.js
@@ -1,45 +1,8 @@
-import readline from "readline";
-import generateContacts from "./scripts/generateContacts.js";
-import addOneContact from "./scripts/addOneContact.js";
-import getAllContacts from "./scripts/getAllContacts.js";
-import countContacts from "./scripts/countContacts.js";
-import removeLastContact from "./scripts/removeLastContact.js";
-import removeAllContacts from "./scripts/removeAllContacts.js";
+import express from "express";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const app = express();
+
+app.listen(8080, (error)=>{
+  console.log("server started");
+  
 });
-
-console.log(`1`);
-
-rl.question("Введіть номер дії: ", async (answer) => {
-  switch (answer.trim()) {
-    case "1":
-      await generateContacts();
-      break;
-    case "2":
-      await addOneContact();
-      break;
-    case "3":
-      await getAllContacts();
-      break;
-    case "4":
-      await countContacts();
-      break;
-    case "5":
-      await removeLastContact();
-      break;
-    case "6":
-      await removeAllContacts();
-      break;
-    case "0":
-      console.log("Вихід");
-      rl.close();
-      return;
-    default:
-      console.log("Невірна команда");
-  }
-  rl.close();
-});
-
