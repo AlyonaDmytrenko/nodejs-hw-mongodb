731b6aee79790264a8a1484439059e5606188132
unknown <dmytrenkoalyona@gmail.com>
2025-07-14 11:11:58 +0300

    error corrected

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
-dotenv.config();
+dotenv.config({ path: '.env.example' });
 
 const PORT = process.env.PORT || 3000;
 
