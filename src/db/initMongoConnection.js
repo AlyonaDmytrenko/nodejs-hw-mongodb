7feba2f5b7d8f193ed1e0a2f3a03ad58e5543daf
unknown <dmytrenkoalyona@gmail.com>
2025-07-15 19:22:49 +0300

    update

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -18,7 +18,11 @@ export const initMongoConnection = async () => {
   const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
   try {
+<<<<<<< Updated upstream
     await mongoose.connect(uri); // ← без опцій
+=======
+    await mongoose.connect(uri); 
+>>>>>>> Stashed changes
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Mongo connection error:', error);
