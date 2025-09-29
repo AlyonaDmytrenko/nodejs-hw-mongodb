506cdad4a9ec23ea9476b5abaa385b8a9500c7e7
unknown <dmytrenkoalyona@gmail.com>
2025-09-29 12:41:26 +0300

    update

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -95,20 +95,29 @@ export async function refreshController(req, res, next) {
   }
 }
 
+// export async function sendResetEmailController(req, res, next) {
+//   await sendResetEmail(req.body.email);
+
+//   res.json({ status: 200, message: 'Message sent successfully' });
+
+//    try {
+//       await sendResetEmail(req.body.email);
+
+//        res.json({ status: 200, message: 'Message sent successfully' });
+//     } catch (error) {
+//        next(error);
+//      }
+// }
+
+
 export async function sendResetEmailController(req, res, next) {
-  await sendResetEmail(req.body.email);
-
-  res.json({ status: 200, message: 'Message sent successfully' });
-
-   try {
-      await sendResetEmail(req.body.email);
-
-       res.json({ status: 200, message: 'Message sent successfully' });
-    } catch (error) {
-       next(error);
-     }
+  try {
+    await sendResetEmail(req.body.email);
+    res.json({ status: 200, message: 'Message sent successfully' });
+  } catch (error) {
+    next(error);
+  }
 }
-
 export async function resetPwdController(req, res, next) {
   const { token, password } = req.body;
 
--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -4,16 +4,15 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false,
+  port: parseInt(process.env.SMTP_PORT, 10),
+  secure: process.env.SMTP_PORT, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
 });
 
-export function sendMail(mail){
-
-    mail.from = "dmytrenkoalyona@gmail.com";
-    return transporter.sendMail(mail);
+export function sendMail(mail) {
+  mail.from = "dmytrenkoalyona@gmail.com";
+  return transporter.sendMail(mail);
 }
