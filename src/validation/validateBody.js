b2e396c3f8bf5f1f2e0f9faec6f3f6251b2475ce
unknown <dmytrenkoalyona@gmail.com>
2025-10-01 15:55:08 +0300

    update

--- src/validation/validateBody.js
+++ src/validation/validateBody.js
@@ -1,21 +1,43 @@
 import createHttpError from "http-errors";
+
 export function validateBody(schema) {
+  return async (req, res, next) => {
+    try {
+      const value = await schema.validateAsync(req.body, {
+        abortEarly: false,
+        stripUnknown: true, 
+      });
 
-    return async (req, res, next) => {
-
-        try {
-            await schema.validateAsync(req.body, {
-                abortEarly: false,
-            });
-
-            next();
-
-        } catch (error) {
-            const errors = error.details.map(detail => detail.message);
-
-            next(new createHttpError.BadRequest(errors));
-
-        }
-
-    };
+      req.body = value; 
+      next();
+    } catch (error) {
+      const errors = error.details.map((detail) => detail.message);
+      next(new createHttpError.BadRequest(errors));
+    }
+  };
 }
+
+
+
+
+// import createHttpError from "http-errors";
+// export function validateBody(schema) {
+
+//     return async (req, res, next) => {
+
+//         try {
+//             await schema.validateAsync(req.body, {
+//                 abortEarly: false,
+//             });
+
+//             next();
+
+//         } catch (error) {
+//             const errors = error.details.map(detail => detail.message);
+
+//             next(new createHttpError.BadRequest(errors));
+
+//         }
+
+//     };
+// }
