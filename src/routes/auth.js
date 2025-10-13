cf9d645216e16bdbe12369ce5ce7114726e27f04
unknown <dmytrenkoalyona@gmail.com>
2025-10-13 15:34:13 +0300

    update

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -40,9 +40,15 @@ router.post(
 );
 
 router.post(
+<<<<<<< Updated upstream
   '/reset-pwd',
   validateBody(resetPwdSchema),
   ctrlWrapper(resetPwdController),
+=======
+  '/reset-password',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+>>>>>>> Stashed changes
 );
 
 router.get(
